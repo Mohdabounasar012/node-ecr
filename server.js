6d1e88c88d0687f69f3ce305931ac7187d12b2cf
Mohd Abou Nasar <abounasar021@gmail.com>
2025-01-25 11:45:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, How Are You Team all good!\n');
+  res.end('Hello, How Are You Team !\n');
 });
 
 server.listen(port, hostname, () => {
